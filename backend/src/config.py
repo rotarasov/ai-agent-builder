8ad18f7b1bc03f15fb74cdf045cf3a8b3723521c
Roman Tarasov <rm.tarasv@gmail.com>
2025-09-26 16:48:30 +0200

    Update config

--- backend/src/config.py
+++ backend/src/config.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config(BaseSettings):
+    base_api_url: str = os.getenv("BASE_API_URL", "http://localhost:8000")
     openai_api_key: str = os.getenv("OPENAI_API_KEY", "<openai_api_key>")
     swiss_ai_platform_api_key: str = os.getenv("SWISS_AI_PLATFORM_API_KEY", "<swiss_ai_platform_api_key>")
     leapcell_api_key: str = os.getenv("LEAPCELL_API_KEY", "<leapcell_api_key>")
